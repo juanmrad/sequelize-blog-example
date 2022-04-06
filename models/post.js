cc4f30529e30fd5f59015df821682aff9c703d83
Juan Mrad <juansmrad@gmail.com>
2022-04-05 20:03:20 -0500

    fix uppercase

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.User = post.belongsTo(models.user, {
+      this.user = post.belongsTo(models.user, {
         foreignKey: 'user_id'
       });
     }
